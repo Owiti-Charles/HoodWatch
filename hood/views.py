414d2f187a37332c4a2e6a52ca475c1a5691e487
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-25 17:54:37 +0300

    Add a logout button to enable users logout from the application

--- hood/views.py
+++ hood/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
 

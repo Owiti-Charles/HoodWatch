d2dca20b332b8b8370dca37ddfdef6fbee379036
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-25 17:17:47 +0300

    Create a signup form

--- hood/urls.py
+++ hood/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register/', views.SignupForm, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    return render(request, 'registration/signup.html')

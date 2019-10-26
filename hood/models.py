55a4ee679e1da79b77de913b17f1f3df6d91fa55
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-26 11:07:52 +0300

    Create neigbourhood model

--- hood/models.py
+++ hood/models.py
@@ -5,11 +5,23 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    logo = models.ImageField(upload_to='images/', default='hoodlogo.png')
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, null=True, on_delete=models.CASCADE, related_name='people')
 
     def __str__(self):
         return f'{self.user.username} profile'
@@ -22,3 +34,6 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+
+

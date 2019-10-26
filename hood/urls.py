495b1bcd02459dd34d676875dc192f820001217b
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-26 11:43:21 +0300

    Create all-hoods tamplate to display all the woods available

--- hood/urls.py
+++ hood/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood')
 ]
--- hood/views.py
+++ hood/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood
 
 
 @login_required(login_url='login')
@@ -22,3 +23,12 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
+def hoods(request):
+    all_hoods = NeighbourHood.objects.all()
+
+    params = {
+        'all_hoods': all_hoods
+    }
+    return render(request, 'all_hoods.html', params)

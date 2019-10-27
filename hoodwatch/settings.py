7772538027b8762222521787cf2b6cdca7850242
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-27 13:26:53 +0300

    Update settings

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
